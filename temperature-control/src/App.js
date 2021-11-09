0337505ad987321cccb81791581963ba5fe38790
victor neves <victorneves15@gmail.com>
2021-11-08 21:26:32 -0300

    Second Commit

---
+++ temperature-control/src/App.js
@@ -0,0 +1,40 @@
+import React, { useState } from 'react';
+import './App.css';
+
+
+function App() {
+  // state hooks
+  const [temperatureValue, setTemperatureValue ] = useState(10);
+
+  const [temperatureColor, setTemperatureColor ] = useState('cold');
+
+  const increaseTemperature = () => {
+    const newTemperature = temperatureValue + 1;
+    setTemperatureValue(newTemperature);
+    if(newTemperature >= 15){
+      setTemperatureColor('hot');
+    }
+  }
+
+  const decreaseTemperature = () => {
+    const newTemperature = temperatureValue - 1;
+    setTemperatureValue(newTemperature);
+    if(newTemperature < 15){
+      setTemperatureColor('cold');
+    }
+  }
+
+  return (      
+      <div className='app-container'>
+          <div className='temperature-display-container'>
+          <div className={`temperature-display ${temperatureColor}`}>{temperatureValue}°C</div>
+        </div>
+        <div className='button-container'>
+          <button onClick={increaseTemperature}>+</button>
+          <button onClick={decreaseTemperature}>-</button>
+        </div>
+      </div>
+  );
+}
+
+export default App;
